a2c1219c52810368816faedfccd380c3d61a6e71
Cédric Lampron <cedric_lampron@hotmail.com>
2024-01-09 18:51:04 -0500

    Handle null

---
+++ Lurker.Epic.Console/Program.cs
@@ -0,0 +1,10 @@
+﻿using Lurker.Epic.Services;
+
+var service = new EpicService();
+
+await service.InitializeAsync();
+
+var games = service.FindGames();
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
--- src/Lurker.Epic/Services/EpicService.cs
+++ src/Lurker.Epic/Services/EpicService.cs
@@ -5,41 +5,45 @@ using Lurker.Common.Extensions;
 using Lurker.Common.Services;
 using Lurker.Epic.Models;
 
-namespace Lurker.Epic.Services
+namespace Lurker.Epic.Services;
+
+public class EpicService : ServiceBase<EpicGame>
 {
-    public class EpicService : ServiceBase<EpicGame>
+    protected override string ProcessName => "EpicGamesLauncher";
+
+    protected override string OpenUrl => "com.epicgames.launcher://open";
+
+    public override List<EpicGame> FindGames()
     {
-        protected override string ProcessName => "EpicGamesLauncher";
-
-        protected override string OpenUrl => "com.epicgames.launcher://open";
-
-        public override List<EpicGame> FindGames()
+        var games = new List<EpicGame>();
+        if (string.IsNullOrEmpty(ExecutablePath))
         {
-            var games = new List<EpicGame>();
-            if (string.IsNullOrEmpty(ExecutablePath))
-            {
-                return games;
-            }
-
-            var localAppdata = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
-            var configurationFile = Path.Combine(localAppdata, "EpicGamesLauncher\\Saved\\Config\\Windows\\GameUserSettings.ini");
-            var text = File.ReadAllText(configurationFile);
-            var installationFolder = text.GetLineAfter("DefaultAppInstallLocation=");
-
-            foreach (var folder in Directory.GetDirectories(installationFolder))
-            {
-                if (!Directory.Exists(Path.Combine(folder, ".egstore")))
-                {
-                    continue;
-                }
-
-                var epicGame = new EpicGame(folder, ExecutablePath);
-                epicGame.Initialize();
-
-                games.Add(epicGame);
-            }
-
             return games;
         }
+
+        var localAppdata = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+        var configurationFile = Path.Combine(localAppdata, "EpicGamesLauncher\\Saved\\Config\\Windows\\GameUserSettings.ini");
+        var text = File.ReadAllText(configurationFile);
+        var installationFolder = text.GetLineAfter("DefaultAppInstallLocation=");
+
+        if (string.IsNullOrEmpty(installationFolder))
+        {
+            return games;
+        }
+
+        foreach (var folder in Directory.GetDirectories(installationFolder))
+        {
+            if (!Directory.Exists(Path.Combine(folder, ".egstore")))
+            {
+                continue;
+            }
+
+            var epicGame = new EpicGame(folder, ExecutablePath);
+            epicGame.Initialize();
+
+            games.Add(epicGame);
+        }
+
+        return games;
     }
 }
